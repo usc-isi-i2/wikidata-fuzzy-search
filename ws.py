2d7e23ff864bda0f5b7c7ee6527596d3cc6d25e3
GreatYYX <i@yyx.me>
2019-11-26 16:13:49 -0800

    cache prepare_dataset

--- ws.py
+++ ws.py
@@ -2,6 +2,7 @@ import os
 import sys
 import glob
 import json
+import pickle
 from SPARQLWrapper import SPARQLWrapper, JSON
 
 from flask import Flask, request, jsonify
@@ -12,24 +13,45 @@ sys.path.append('data-label-augmentation')
 sys.path.append('data-label-augmentation/src/label_augmenter/')
 WORD2VEC_MODEL_PATH = 'data-label-augmentation/data/GoogleNews-vectors-negative300-SLIM.bin'
 WD_QUERY_ENDPOINT = 'http://dsbox02.isi.edu:8888/bigdata/namespace/wdq/sparql'
+CONFIG_PATH = 'cfg/wikidata.yml'
+DATA_PATH = 'data/wikidata.json'
 
 from linking_script import *
 
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
-configs = {}
+config = {}
 resources = {}
 
-CONFIG_DIR_PATH = os.path.abspath(os.path.join('cfg/', '*.yml'))
 sparql_endpoint = SPARQLWrapper(WD_QUERY_ENDPOINT)
+ADMIN_SUB_LEVELS = [None, 'Q10864048', 'Q13220204', 'Q13221722']
 
-with open('data/wikidata.json') as f:
+with open(DATA_PATH) as f:
     all_pnodes = json.loads(f.read())
 
 
 class WikidataLinkingScript(LinkingScript):
-    def process(self, keywords, country):
+    def load_from_cache(self, cache_file, func, *args):
+        if os.path.exists(cache_file):
+            with open(cache_file, 'rb') as f:
+                r = pickle.load(f)
+        else:
+            r = func(*args)
+            if cache_file:
+                with open(cache_file, 'wb') as f:
+                    pickle.dump(r, f)
+        return r
+
+    def prepare_datasets(self, 
+            target_data=None, target_data_filtered=None, 
+            target_data_aug=None, target_data_aug_filtered=None):
+        self.target_data = self.load_from_cache(target_data, self.load_target_data)
+        self.target_data_filtered = self.load_from_cache(target_data_filtered, self.filter_target_data, self.target_data)
+        self.target_data_aug = self.load_from_cache(target_data_aug, self.augment_target_data, self.target_data_filtered)
+        self.target_data_aug_filtered  = self.load_from_cache(target_data_aug_filtered, self.filter_augmented_target_data, self.target_data_aug)
+
+    def process(self, keywords, country, sub_level):
         self.source_data = self.load_source_data(keywords)
         self.source_data_filtered = self.filter_source_data(self.source_data)
         self.source_data_aug = self.augment_source_data(self.source_data_filtered)
@@ -58,6 +80,7 @@ class WikidataLinkingScript(LinkingScript):
                         alignedmap['qualifiers'] = get_qualifiers(country, pnode)
                         if alignedmap['time']:
                             alignedmap['statistics'] = get_statistics(country, pnode, alignedmap['time'])
+                        # alignedmap['subdivisions'] = get_sub_levels(country, sub_level)
                     else:
                         alignedmap["name"] = aligned["wl"].get_original_string()
                 alignedmap["score"] = aligned["score"]
@@ -128,68 +151,87 @@ SELECT (max(?time) as ?max_time) (min(?time) as ?min_time) (count(?time) as ?cou
         statistics['max_precision'] = int(max_precision) if max_precision else None
     return statistics
 
+def get_sub_levels(country, sub_level):
+    if sub_level == 0:
+        return {}
+
+    query = '''
+SELECT DISTINCT ?sub_no_prefix ?subLabel WHERE {
+  ?sub p:P31/ps:P31 ?level .
+  ?level p:P279/ps:P279 wd:''' + ADMIN_SUB_LEVELS[sub_level] + ''' . 
+  ?level p:P17/ps:P17 wd:''' + country + '''.
+  BIND (STR(REPLACE(STR(?sub), STR(wd:), "")) AS ?sub_no_prefix) .
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en" }
+}'''
+
+    sparql_endpoint.setQuery(query)
+    sparql_endpoint.setReturnFormat(JSON)
+    results = sparql_endpoint.query().convert()
+
+    subs = {}
+    for result in results["results"]["bindings"]:
+        subs[result['sub_no_prefix']['value']] = result['subLabel']['value']
+
+    return subs
+
 
 def load_resources():
     # preload resources
     print('loading resources...')
     resources['GNews_SLIM_model'] = load_word2vec_model(WORD2VEC_MODEL_PATH, binary=True)
 
-    # configs
-    for config_path in glob.glob(CONFIG_DIR_PATH):
-        print('loading config:', config_path)
-        k = os.path.splitext(os.path.basename(config_path))[0]
-        configs[k] = {
-            'abspath': config_path,
-            'config': make_YML_config(config_path),
-            'script': {
-                'alignment': None,
-                'linking': None
-            }
+    # config
+    config = {
+        'config': make_YML_config(CONFIG_PATH),
+        'script': {
+            'alignment': None,
+            'linking': None
         }
-        configs[k]['config'].set_augmenter_preload_resources(resources)
+    }
+    config['config'].set_augmenter_preload_resources(resources)
 
     # load datasets
-    for k in configs.keys():
-        print('loading datasets...', k)
-        script = WikidataLinkingScript(configs[k]['config'])
-        script.prepare_datasets()
-        configs[k]['script']['linking'] = script
+    print('loading datasets...')
+    script = WikidataLinkingScript(config['config'])
+    script.prepare_datasets(
+        target_data='data/wikidata_target_data.pkl', 
+        target_data_filtered='data/wikidata_target_data_filtered.pkl', 
+        target_data_aug='data/wikidata_target_data_aug.pkl', 
+        target_data_aug_filtered='data/wikidata_target_data_aug_filtered.pkl')
+    config['script']['linking'] = script
+
 
 class ApiRoot(Resource):
     def get(self):
-        return 'Data augmentation web service'
-
-class ApiConfig(Resource):
-    def get(self):
-        return list(configs.keys())
+        return 'Wikidata Fuzzy Search'
 
 class ApiLinking(Resource):
-    def get(self, config_name):
-        if not config_name in configs:
-            return {'error': 'Invalid config name'}, 500
-        # wordmap = request.args.get('wordmap', default=False, type=bool)
+    def get(self):
         keywords = request.args.get('keywords', None)
         if not keywords:
             return {'error': 'Invalid keywords'}, 400
-
         keywords = list(map(lambda x: x.strip(), keywords.split(',')))
+
         country = request.args.get('country', None)
         if not country:
             return {'error': 'Invalid country'}, 400
 
-        script = configs[config_name]['script']['linking']
-        # if wordmap:
-        #     return script.get_word_map(keywords)
+        sub_level = request.args.get('sub_level', 0)
+        try:
+            sub_level = int(sub_level)
+            if not (0 <= sub_level < len(ADMIN_SUB_LEVELS)):
+                raise Exception
+        except:
+            return {'error': 'Invalid sub_level'}, 400
 
-        align_list = script.process(keywords, country)
+        script = config['script']['linking']
+        align_list = script.process(keywords, country, sub_level)
         return align_list
 
 
-
 api.add_resource(ApiRoot, '/')
-api.add_resource(ApiConfig, '/config')
-api.add_resource(ApiLinking, '/linking/<string:config_name>')
+api.add_resource(ApiLinking, '/linking')
 
 if __name__ == '__main__':
     load_resources()
-    app.run(debug=False, host="0.0.0.0", port=14000)
+    app.run(debug=False, host="0.0.0.0", port=14001)
