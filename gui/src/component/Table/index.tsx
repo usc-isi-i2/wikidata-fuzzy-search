9c2b4397e5bdafaad6fddec836ec27019294efc5
Daniella <daniellaillouz@gmail.com>
2019-12-30 16:28:07 +0200

    table component update git ignore file

---
+++ gui/src/component/Table/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import wikiStore from "../../data/store";
+
+// FontAwesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChartBar, faExternalLinkSquareAlt, faTable, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
+
+import * as wikidataQuery from '../../wikidataQuery';
+import { observer } from 'mobx-react';
+
+@observer
+export default class Table extends React.Component<{}, {}>{
+
+    render() {
+        return (
+            <div className="h-100" style={{ overflow: 'hidden' }}>
+
+                {/* iframe */}
+                <iframe
+                    title="preview"
+                    style={{ border: 'none', width: '100%', height: 'calc(100% - 44px)', marginTop: '44px' }}
+                    src={wikiStore.iframeSrc}
+                    key={wikiStore.iframeSrc} // used to force re-render this iframe
+                    referrerPolicy="origin"
+                    sandbox="allow-scripts allow-same-origin allow-popups"
+                />
+
+            </div>
+        );
+    }
+}
