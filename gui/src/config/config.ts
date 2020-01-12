a8a39496b5c02c2e9f6b23701fd783fccd83b84c
Itay Zandbank <itay@chelem.co.il>
2020-01-12 12:34:02 +0200

    Remove the old iframe-based query code

--- gui/src/config/config.ts
+++ gui/src/config/config.ts
@@ -2,14 +2,12 @@
 interface Configuration {
     isDebugging: boolean,
     backendServer: string,
-    queryServer: string,
     sparqlQuery: string,
 }
 
 const config = {
     isDebugging: process.env.REACT_APP_IS_DEBUGGING == 'true',
     backendServer: process.env.REACT_APP_BACKEND_URL,
-    queryServer: "https://dsbox02.isi.edu:8888",
     sparqlQuery: process.env.REACT_APP_SPARQL_URL,
 } as Configuration;
 
