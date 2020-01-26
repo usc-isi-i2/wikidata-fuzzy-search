c2655d677a77ab02b1773d86a480bcbcdbe792cc
Daniella <daniellaillouz@gmail.com>
2020-01-26 10:40:45 +0200

    change colors for scatterPlot

--- frontend/src/customizations/plots.tsx
+++ frontend/src/customizations/plots.tsx
@@ -22,7 +22,7 @@ export const markerSymbols = [
     'y-right', 'y-right-open', 'line-ew', 'line-ew-open', 'line-ns', 'line-ns-open', 'line-ne', 'line-ne-open', 'line-nw', 'line-nw-open'
 ]
 
-export const colors = ['blue', 'cyan', 'red', 'yellow', 'green', 'grey', 'black', 'brown', 'purple', 'orange'];
+export const colors = ["#1f78b4","#b2df8a","#33a02c","#fb9a99","#e31a1c","#fdbf6f","#ff7f00","#cab2d6","#6a3d9a","#b15928"];
 
 export const lineStyles = ['solid', 'dot', 'dash', 'longdash', 'dashdot', 'longdashdot']
 
